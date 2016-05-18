f9fb76d68a5d8dc27c6eec56fc14cbcfe3ed3033
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2016-05-18 20:47:10 +1000

    Update cl_init.lua

--- playerjoined/lua/playerjoined/cl_init.lua
+++ playerjoined/lua/playerjoined/cl_init.lua
@@ -1,7 +1,7 @@
 --
 -- Copyright (c) 2016 by Aegis Computing. All Rights Reserved.
 --
-// Yes, the copyright is shitty but it protects my work. So there's that.
+// Yes, the copyright is shitty, but it protects my work, so there's that.
 
 
 // Don't even try it, you sneaky asshole.
